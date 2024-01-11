0d8863a31bf567fb3e639740a5ead92bbe55dcc2
rahuldevadigaq <156284361+rahuldevadigaq@users.noreply.github.com>
2024-01-11 12:39:45 +0530

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from google.cloud import bigquery
+'''
+# Set your GCP project ID
+project_id = 'q-gcp-00863-fsihackathon-23-12'
+
+# Set your BigQuery dataset and table
+dataset_id = 'nba_synthesizer_forecast_modifier'
+table_id = 'forecasted_output_firstpass'
+
+# Create a BigQuery client
+client = bigquery.Client(project=project_id)
+
+# Query to retrieve data from BigQuery table
+query = f'SELECT * FROM `{project_id}.{dataset_id}.{table_id}`'
+
+# Execute the query
+query_job = client.query(query)
+
+# Fetch the results
+results = query_job.result()
+
+# Display the results in Streamlit
+st.write("## BigQuery Table Contents:")
+for row in results:
+    st.write(row)
+
+'''
+t.write('hello')
